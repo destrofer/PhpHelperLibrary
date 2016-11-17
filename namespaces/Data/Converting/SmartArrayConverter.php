ec2d352efc45741900e054a890894076cff0bf8d
Viacheslav Soroka <alpha@balticdev.com>
2016-11-17 13:05:27 +0200

    Added \Data\Converting\SmartArrayConverter class.

---
+++ namespaces/Data/Converting/SmartArrayConverter.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * Copyright 2016 Viacheslav Soroka
+ * Licensed under GNU Lesser General Public License v3.
+ * See LICENSE in repository root folder.
+ * @link https://github.com/destrofer/PhpHelperLibrary
+ */
+
+namespace Data\Converting;
+
+/**
+ * Converts arrays to objects and vice versa.
+ *
+ * The converter fills in only existing properties of given instances (except stdClass objects). It uses information
+ * from doc blocks to determine how the imported or exported data should actually be treated.
+ */
+class SmartArrayConverter
+{
+	/**
+	 * Fills in information form given array into the given object.
+	 *
+	 * @param string|object $newObject An instance of an existing object or a class name for a new instance of an object that must be filled in from array. If parameter is string new instance is created without calling a constructor. If parameter is an instance of an object then the same instance is returned by this method.
+	 * @param array $array
+	 * @return object
+	 */
+	public static function fillObjectFromArray($newObject, $array) {
+		if( is_string($newObject) ) {
+			$class = new \ReflectionClass($newObject);
+			$newObject = $class->newInstanceWithoutConstructor();
+		}
+		if( $newObject instanceof \stdClass ) {
+			foreach( $array as $propertyName => &$value )
+				$newObject->$propertyName = is_array($value) ? (object)$value : $value;
+		}
+		else {
+			$class = new \ReflectionClass($newObject);
+			foreach( $class->getProperties() as $property ) {
+				$propertyName = null;
+				$docBlock = $property->getDocComment();
+				if( $docBlock && preg_match("#@import(?:[ \t]+(?:([^\"\'][^\\s]*)|\"(.*)\"|\'(.*)\'))?#imu", $docBlock, $mtc) )
+					$propertyName = isset($mtc[3]) ? $mtc[3] : (isset($mtc[2]) ? $mtc[2] : (isset($mtc[1]) ? $mtc[1] : $property->getName()));
+				else
+					continue;
+				if( !array_key_exists($propertyName, $array) )
+					continue;
+				$value = is_object($array) ? $array->$propertyName : $array[$propertyName];
+				if( (is_array($value) || is_object($value)) && $docBlock && preg_match('#@var\\s+([\\\\a-z0-9_]+)#isu', $docBlock, $mtc) ) {
+					if( $mtc[1] == 'object' )
+						$mtc[1] = '\\stdClass';
+					$className = class_exists($mtc[1]) ? $mtc[1] : ($class->getNamespaceName() . '\\' . $mtc[1]);
+					if( class_exists($className) ) {
+						if( method_exists($className, 'fromArray') )
+							$property->setValue($newObject, call_user_func($className . '::fromArray', $value));
+						else
+							$property->setValue($newObject, self::fillObjectFromArray($className, $value));
+					}
+					else
+						$property->setValue($newObject, $value);
+				}
+				else
+					$property->setValue($newObject, $value);
+			}
+		}
+		return $newObject;
+	}
+
+	/**
+	 * Converts given object to an array.
+	 *
+	 * @param object $object
+	 * @return array
+	 */
+	public static function objectToArray($object) {
+		$array = [];
+		if( $object instanceof \stdClass ) {
+			foreach( $object as $propertyName => $value ) {
+				if( is_object($value) ) {
+					if( method_exists($value, 'toArray') )
+						$value = call_user_func([$value, 'toArray']);
+					else
+						$value = self::objectToArray($value);
+				}
+				$array[$propertyName] = $value;
+			}
+		}
+		else {
+			$class = new \ReflectionClass($object);
+			foreach( $class->getProperties() as $property ) {
+				$docBlock = $property->getDocComment();
+				if( $docBlock && preg_match("#@export(?:[ \t]+(?:([^\"\'][^\\s]*)|\"(.*)\"|\'(.*)\'))?#imu", $docBlock, $mtc) )
+					$propertyName = isset($mtc[3]) ? $mtc[3] : (isset($mtc[2]) ? $mtc[2] : (isset($mtc[1]) ? $mtc[1] : $property->getName()));
+				else
+					continue;
+				$value = $property->getValue($object);
+				if( is_object($value) ) {
+					if( method_exists($value, 'toArray') )
+						$value = call_user_func([$value, 'toArray']);
+					else
+						$value = self::objectToArray($value);
+				}
+				$array[$propertyName] = $value;
+			}
+		}
+		return $array;
+	}
+}

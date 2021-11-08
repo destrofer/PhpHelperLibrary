3225565bb4b256d62e23c392b6ded9431d1531b1
Viacheslav Soroka <alpha@balticdev.com>
2021-11-08 09:21:18 +0200

    Fix part of HTTP header getting duplicated in Downloader::beginDownload().

--- namespaces/Destrofer/Web/Downloader.php
+++ namespaces/Destrofer/Web/Downloader.php
@@ -186,7 +186,6 @@ class Downloader {
 			$type = (($postData !== null) ? "POST" : "GET");
 
 		$headers = [];
-		$headers[''] = "$type $path HTTP/1.1";
 		$headers['Host'] = $host;
 		$headers['User-Agent'] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36";
 
